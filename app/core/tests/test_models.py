2b152da554db35fe7edb1fb5febe3b9a05a502ca
Amer Hidanovic <amer.hidanovic@gmail.com>
2022-04-25 14:28:05 +0200

    Updated admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,7 +29,6 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
-
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
@@ -39,5 +38,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        
-
